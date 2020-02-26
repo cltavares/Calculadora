ea2899730774fd4fdfa888877304dd6d597125f6
cltavares <cltavares@gmail.com>
2020-02-26 20:20:38 -0300

    first

---
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.calculadora
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var botao = findViewById<Button>(R.id.button2);
+
+        var text1 = findViewById<EditText>(R.id.editText8);
+
+        var text2 = findViewById<EditText>(R.id.editText10);
+
+        var textofinal = findViewById<EditText>(R.id.editText12);
+
+
+        button3.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() - text2.text.toString().toDouble()).toString())};
+        button5.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() * text2.text.toString().toDouble()).toString())};
+        button4.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() / text2.text.toString().toDouble()).toString())};
+        button2.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() + text2.text.toString().toDouble()).toString())};
+
+    }
+}
