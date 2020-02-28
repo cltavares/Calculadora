14eafbc91839629fddb936f459b4bf5c95b4e757
cltavares <cltavares@gmail.com>
2020-02-28 14:21:01 -0300

    ajuste resultado

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,31 +1,56 @@
 package com.example.calculadora
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        var botao = findViewById<Button>(R.id.button2);
+        val botao2 = findViewById<AppCompatButton>(R.id.button2);
+        val botao4 = findViewById<AppCompatButton>(R.id.button4);
+        val botao3 = findViewById<AppCompatButton>(R.id.button3);
+        val botao5 = findViewById<AppCompatButton>(R.id.button5);
+        //var botao5 = findViewById(R.id.button5) as Button
 
-        var text1 = findViewById<EditText>(R.id.editText8);
+        val text1 = findViewById<AppCompatEditText>(R.id.editText8);
 
-        var text2 = findViewById<EditText>(R.id.editText10);
+        val text2 = findViewById<AppCompatEditText>(R.id.editText12);
 
-        var textofinal = findViewById<EditText>(R.id.editText12);
+        val textofinal = findViewById<AppCompatEditText>(R.id.editText10);
 
 
-        button3.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() - text2.text.toString().toDouble()).toString())};
-        button5.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() * text2.text.toString().toDouble()).toString())};
-        button4.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() / text2.text.toString().toDouble()).toString())};
-        button2.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() + text2.text.toString().toDouble()).toString())};
+
+        botao3.setOnClickListener {
+            // make a toast on button click event
+            Toast.makeText(this, "Hi there! This is a Toast.", Toast.LENGTH_LONG).show()
+        }
+
+        botao5.setOnClickListener {
+            // make a toast on button click event
+
+            val text1: Double = text1.getText().toString().toDouble()
+            val text2: Double = text2.getText().toString().toDouble()
+
+            var valMultiply: Double = text1 * text2;
+
+            Toast.makeText(this, "Hi multiply."+valMultiply, Toast.LENGTH_LONG).show()
+            textofinal.setText( valMultiply.toString());
+        }
+
+/*
+        botao3.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() - text2.text.toString().toDouble()).toString())};
+        botao5.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() * text2.text.toString().toDouble()).toString())};
+        botao4.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() / text2.text.toString().toDouble()).toString())};
+        botao2.setOnClickListener{textofinal.setText((text1.text.toString().toDouble() + text2.text.toString().toDouble()).toString())};
+*/
+
 
     }
 }
